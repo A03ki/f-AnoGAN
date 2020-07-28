7f1114046b356e0ee27ca475b33b5042e3cb9a27
A03ki <a03ki04@gmail.com>
2020-07-29 08:14:37 +0900

    Add a progress bar

--- fanogan/test_anomaly_detection.py
+++ fanogan/test_anomaly_detection.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.utils.model_zoo import tqdm
 
 
 def test_anomaly_detection(opt, generator, discriminator, encoder,
@@ -17,7 +18,7 @@ def test_anomaly_detection(opt, generator, discriminator, encoder,
     with open("results/score.csv", "w") as f:
         f.write("label,img_distance,anomaly_score,z_distance\n")
 
-    for (img, label) in dataloader:
+    for (img, label) in tqdm(dataloader):
 
         real_img = img.to(device)
 
