c431034f818c9c9577c0ecac5d9390a9293c4661
A03ki <a03ki04@gmail.com>
2020-08-01 19:13:22 +0900

    Add python_requires

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version="0.0.1",
     description="Test f-AnoGAN by using datasets.",
     author="A03ki",
+    python_requires=">=3.6",
     install_requires=["torch", "torchvision", "torchvision4ad", "matplotlib",
                       "numpy", "pandas", "Pillow", "scikit-learn"],
     url="https://github.com/A03ki/f-AnoGAN",
